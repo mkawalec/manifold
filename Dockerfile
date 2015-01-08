70f40712765cce64f5f9b986d0e1d0f544281d6d
Michał Kawalec <michal@bazzle.me>
2015-01-08 12:38:03 +0100

    an initial build system is created

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM dockerfile/nodejs
+
+MAINTAINER Michal Kawalec <michal@bazzle.me>
+
+ENV NODE_ENV development
+
+RUN sudo npm install -g gulp knex bower 
+RUN sudo apt-get update && sudo apt-get install -y libfreetype6 libfontconfig1
+
+RUN mkdir /code
+VOLUME /code
+ADD . /code
+WORKDIR /code
+
+RUN npm install
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var reactify = require('reactify');
+
+
+var getBundleName = function () {
+  var version = require('./package.json').version;
+  var name = require('./package.json').name;
+  return version + '.' + name + '.' + 'min';
+};
+
+gulp.task('build', function() {
+
+  var bundler = browserify({
+    entries: [ './client' ],
+    extensions: [ '.js', '.jsx' ],
+    debug: true
+  }).transform(reactify);
+
+  var bundle = function() {
+    return bundler
+      .bundle()
+      .pipe(source(getBundleName() + '.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./client/build/js/'));
+  };
+
+  return bundle();
+});
